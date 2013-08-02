6ea7cea2a4d2a1584b51e9052220cdf3e0678136
Tom Anderson <tom.h.anderson@gmail.com>
2013-08-02 15:19:13 -0700

    init commit

---
+++ Module.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Cloudflare;
+
+use Zend\ModuleManager\ModuleManager;
+
+class Module
+{
+    private $eventManager;
+
+    public function init(ModuleManager $manager)
+    {
+        if(isset($_SERVER['HTTP_CF_CONNECTING_IP']))
+        {
+            $_SERVER['REMOTE_ADDR'] = $_SERVER['HTTP_CF_CONNECTING_IP'];
+        }
+
+        if(isset($_SERVER['HTTP_CF_VISITOR']))
+        {
+            $visitor = json_decode($_SERVER['HTTP_CF_VISITOR']);
+
+            if($visitor->scheme === 'https')
+            {
+                $_SERVER['HTTPS'] = 'on';
+                $_SERVER['HTTP_X_FORWARDED_PROTO'] = 'https';
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+Cloudflare Module for ZF2
+=========================
+
+This module adjusts the global variables $_SERVER['REMOTE_ADDR'], $_SERVER['HTTPS'], and $_SERVER['HTTP_X_FORWARDED_PROTO'] to their correct values when using Cloudflare.
+
+Install
+-------
+
+```php
+php composer.phar require "stuki/cloudflare": "dev-master"
+```
+
+
+Enable module in `config/application.config.php`: 
+```php
+return array(
+    'modules' => array(
+        'Cloudflare',
+        ...
+    ),
+```
