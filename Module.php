4126706051362629d42eac64b04ab1579286ad96
Tom Anderson <tom.h.anderson@gmail.com>
2014-09-15 11:28:16 -0600

    Correct port for https

--- Module.php
+++ Module.php
@@ -23,6 +23,7 @@ class Module
             {
                 $_SERVER['HTTPS'] = 'on';
                 $_SERVER['HTTP_X_FORWARDED_PROTO'] = 'https';
+                $_SERVER['SERVER_PORT'] = '443';
             }
         }
     }
